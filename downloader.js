cddbdc1a7cb10f504abc5d1f502e4e688018d06a
Tyler Manning <tylerericmanning@gmail.com>
2018-02-11 23:16:45 -0500

    Opens a new window

--- downloader.js
+++ downloader.js
@@ -1,4 +1,4 @@
-// Format for NMM Download Link
+// Format for NMM Download url
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000172397&nmm=1&game_id=110&source=ModPage - SkyUI
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000183703&nmm=1&game_id=110&source=ModPage - Immersive Armors
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000023285&nmm=1&game_id=110&source=ModPage - Immersive Weapons
@@ -6,27 +6,32 @@
 // Compiles an array of all the mods on screen
 var modList = document.getElementsByClassName("btnexpand");
 
-var items = modList[0].getElementsByTagName("li");
-var ul = items[0].getElementsByTagName("ul");
-var listItems = ul[0].getElementsByTagName("li");
+for (let i = 0; i < modList.length; i++) {
 
-var link = listItems[0].getElementsByTagName('a')[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
-console.log(link);
+    var items = modList[i].getElementsByTagName("li");
+    var ul = items[0].getElementsByTagName("ul");
+    var listItems = ul[0].getElementsByTagName("li");
 
+    var url = listItems[0].getElementsByTagName('a')[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
 
+    var li = document.createElement("li");
+    var span = document.createElement("a");
+    span.setAttribute("class", "mod-track");
+    span.appendChild(document.createTextNode("Download Mod"));
 
+    li.setAttribute("id","download");
+    li.appendChild(span);
+    li.addEventListener("click", function() {
+        downloadMod(url);
+    });
 
+    ul[0].appendChild(li);
+   
+}
 
+function downloadMod(url) {
+    var currentWindow = window.location;
+    window.open(url, '_blank');
+    console.log(window.location);
+}
 
-// var li = document.createElement("li");
-// var a = document.createElement("a");
-// a.setAttribute("class", "mod-track");
-// a.appendChild(document.createTextNode("Download Mod"));
-
-
-// li.setAttribute("id","download");
-// li.appendChild(a);
-
-
-
-// ul[0].appendChild(li);
