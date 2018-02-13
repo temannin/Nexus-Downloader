0df9d463760e59d0eaf3697a8d055ef068709415
Tyler Manning <tylerericmanning@gmail.com>
2018-02-13 02:43:41 -0500

    Extension can finally download  mods however does not scale across multiple pages

--- download-handler.js
+++ download-handler.js
@@ -3,30 +3,70 @@ var modPage;
 
 function connected(p) {
     portFromCS = p;
-    
+    console.log("connected");
 
     portFromCS.onMessage.addListener(function (m) {
-        var test = browser.windows.create({
+        console.log(m.greeting);
+
+        var modWindow = browser.tabs.create({
             url: m.greeting,
-            allowScriptsToClose: true,
-            state: "minimized"
+            active: false
         });
-        test.then(onCreated, onError);
+        modWindow.then(onCreated, onError);
 
-        function onCreated(windowInfo) {
-            document.body.style.border = "5px solid red";
-            console.log(windowInfo);
-            portFromCS.postMessage({ greeting: windowInfo.id});
-            // var removed = browser.windows.remove(windowInfo.id);
-          }
-          
-          function onError(error) {
-            console.log(`Error: ${error}`);
-          }
+        function onCreated(tab) {
+            controlTab(tab.id);
+        }
 
+        function onError() {
+            console.log("Error");
+        }
     });
 
+    function controlTab(tabId) {
+        console.log("Sending code to " + tabId);
 
+        function onExecuted(result) {
+            console.log(`We made it green`);
+        }
+
+        function onError(error) {
+            console.log(`Error: ${error}`);
+        }
+
+        var downloadClick = 'var btns = document.getElementsByClassName("btn inline-flex popup-btn-ajax"); btns[0].click()';
+
+        var port = browser.tabs.executeScript(
+            tabId, {
+                code: downloadClick
+            }
+        );
+        port.then(onExecuted, onError);
+
+
+
+    }
+
+    // portFromCS.onMessage.addListener(function (m) {
+    //     console.log(m.greeting);
+    //     var test = browser.windows.create({
+    //         url: m.greeting,
+    //         allowScriptsToClose: true
+    //     });
+    //     test.then(onCreated, onError);
+
+    //     function onCreated(windowInfo) {
+    //         downloadInit(windowInfo.id);
+    //     }
+
+    // });
 }
 
+
+
+function handleMessage(request, sender, sendResponse) {
+    console.log(`${request.content}`);
+    // sendResponse({response: "response from background script"});
+}
 browser.runtime.onConnect.addListener(connected);
+
