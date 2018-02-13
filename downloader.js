bdf0453c117f458b7a13d3ac96a0908235ddd18e
Tyler Manning <tylerericmanning@gmail.com>
2018-02-13 14:07:23 -0500

    Turned the compile list into a function
    The extension needs to be able to replace a list item instead of only just appending to the ul item.

--- downloader.js
+++ downloader.js
@@ -3,32 +3,37 @@
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000183703&nmm=1&game_id=110&source=ModPage - Immersive Armors
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000023285&nmm=1&game_id=110&source=ModPage - Immersive Weapons
 
-var myPort = browser.runtime.connect({ name: "2a1df420b945dc71db85c681379ce0384b820fd0@temporary-addon" });
+var myPort = browser.runtime.connect({ name: "f6d9502276f9fcfa00b6825dec8d9eea9e6ef929@temporary-addon" });
+document.body.addEventListener('click', appendMods, true); 
 
-// Compiles an array of all the mods on screen
-var modList = document.getElementsByClassName("btnexpand");
+appendMods();
 
-for (let i = 0; i < modList.length; i++) {
 
-    var items = modList[i].getElementsByTagName("li");
-    var ul = items[0].getElementsByTagName("ul");
-    var listItems = ul[0].getElementsByTagName("li");
+function appendMods() {
+    // Compiles an array of all the mods on screen
+    var modList = document.getElementsByClassName("btnexpand");
+    for (let i = 0; i < modList.length; i++) {
 
-    var url = listItems[0].getElementsByTagName('a')[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
+        var items = modList[i].getElementsByTagName("li");
+        var ul = items[0].getElementsByTagName("ul");
+        var listItems = ul[0].getElementsByTagName("li");
 
-    var li = document.createElement("li");
-    var span = document.createElement("a");
-    span.setAttribute("class", "mod-track");
-    span.appendChild(document.createTextNode("Download Mod"));
+        var url = listItems[0].getElementsByTagName('a')[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
 
-    li.setAttribute("id", "download");
-    li.appendChild(span);
-    li.addEventListener("click", function () {
-        downloadMod(url);
-    });
+        var li = document.createElement("li");
+        var span = document.createElement("a");
+        span.setAttribute("class", "mod-track");
+        span.appendChild(document.createTextNode("Download Mod"));
 
-    ul[0].appendChild(li);
+        li.setAttribute("id", "download");
+        li.appendChild(span);
+        li.addEventListener("click", function () {
+            downloadMod(url);
+        });
 
+
+        ul[0].appendChild(li);
+    }
 }
 
 function downloadMod(url) {
