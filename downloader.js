d21a2e06ced75c004427a30e14c90ce4e8e70251
Tyler Manning <tylerericmanning@gmail.com>
2018-02-16 10:43:41 -0500

    URLs change when User edits page

--- downloader.js
+++ downloader.js
@@ -3,44 +3,48 @@
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000183703&nmm=1&game_id=110&source=ModPage - Immersive Armors
 // https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=1000023285&nmm=1&game_id=110&source=ModPage - Immersive Weapons
 
-var myPort = browser.runtime.connect({ name: "068db9165395513da5c8c4e87f1fdb08eb00923e@temporary-addon" });
+var myPort = browser.runtime.connect({
+  name: "4e36a53c948953415d7faf918cd6fc0f3649bbcf@temporary-addon"
+});
+document.body.addEventListener("click", compileMods, true);
 
 // Compiles an array of all the mods on screen
 var modList = document.getElementsByClassName("btnexpand");
-var urls = []
+var urls = [];
 
 compileMods();
 
 function compileMods() {
-    urls = [];
-    for (let i = 0; i < modList.length; i++) {
-        var items = modList[i].getElementsByTagName("li");
-        var ul = items[0].getElementsByTagName("ul");
-        var listItems = ul[0].getElementsByTagName("li");
+  urls = [];
+  for (let i = 0; i < modList.length; i++) {
+    var items = modList[i].getElementsByTagName("li");
+    var ul = items[0].getElementsByTagName("ul");
+    var listItems = ul[0].getElementsByTagName("li");
 
-        var url = listItems[0].getElementsByTagName('a')[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
-        urls.push(url);
-        console.log(urls);
+    var url = listItems[0].getElementsByTagName("a")[0].href; // retrieves URL for each independent mod - needs the hardcoded 0
+    urls.push(url);
+    console.log(urls);
 
-        var li = document.createElement("li");
-        var span = document.createElement("a");
-        span.setAttribute("class", "mod-track");
-        span.appendChild(document.createTextNode("Download Mod"));
+    var li = document.createElement("li");
+    var span = document.createElement("a");
+    span.setAttribute("class", "mod-track");
+    span.appendChild(document.createTextNode("Download Mod"));
 
-        li.setAttribute("id", "download" + i);
-        li.appendChild(span);
-        li.addEventListener("click", function () {
-            console.log(i);
-            downloadMod(urls[i]);
-        });
+    li.setAttribute("id", "download" + i);
+    li.appendChild(span);
+    li.addEventListener("click", function() {
+      console.log(i);
+      downloadMod(urls[i]);
+    });
 
-        ul[0].appendChild(li);
-
+    if (document.getElementById("download" + i) == null) {
+      ul[0].appendChild(li);
+    } else {
+        document.getElementById("download" + i) = li;
     }
+  }
 }
 
 function downloadMod(url) {
-    myPort.postMessage({ greeting: url });
+  myPort.postMessage({ greeting: url });
 }
-
-
